51366934f30addbc80ff381fc8627c2b9600909d
J Resilient <j.resilient.1@gmail.com>
2020-09-23 18:32:28 -0400

    Fix #login in application_controller to use a user object instead of username and password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,17 @@ class ApplicationController < ActionController::Base
     helper_method :current_user, :login!, :logout!
 
     def current_user
+        return if session[:session_token].nil?
         @current_user ||= User.find_by(session_token: session[:session_token])
     end
 
-    def login!(username, password)
-        return if @current_user
-        user = User.find_by_credentials(username, password)
-        if user.nil?
-            flash.now[:errors] = ["Incorrect username or password."]
-            return nil
-        end
-        session[:session_token] = user.session_token
+    def login!(user)
         @current_user = user
+        session[:session_token] = user.session_token
     end
 
     def logout!
+        return if session[:session_token].nil?
         @current_user.reset_session_token
         @current_user = nil
         session[:session_token] = nil
