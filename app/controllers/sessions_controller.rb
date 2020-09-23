cb478af63f3ff96b806c8d8c85d648b41c37695b
J Resilient <j.resilient.1@gmail.com>
2020-09-23 18:40:26 -0400

    Add #new, #create, #destroy, and #session_params to sessions_controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+    def new
+        render :new
+    end
+
+    def create
+        user = User.find_by_credentials(
+            session_params[:username],
+            session_params[:password]
+        )
+        if user.nil?
+            flash.now[:errors] = ["Incorrect username or password."]
+            render :new
+        else
+            login!(user)
+            redirect_to user_url(user)
+        end
+    end
+
+    def destroy
+        logout!
+        render :new
+    end
+
+    private
+    def session_params
+        params.require[:session].permit(:username, :password)
+    end
+end
