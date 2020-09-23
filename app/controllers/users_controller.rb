d5efe0fc8faaf428dd452408066ea161c60b633b
J Resilient <j.resilient.1@gmail.com>
2020-09-23 18:35:55 -0400

    Add render to if/else statment in #create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
             redirect_to user_url(user)
         else
             flash.now[:errors] = user.errors.full_messages
+            render :new
         end
     end
 
