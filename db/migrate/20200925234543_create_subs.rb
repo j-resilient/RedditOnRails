e4738245c36f30668368cf6dbbd0625612b1b8e9
J Resilient <j.resilient.1@gmail.com>
2020-09-25 19:48:05 -0400

    Add subs table

---
+++ db/migrate/20200925234543_create_subs.rb
@@ -0,0 +1,9 @@
+class CreateSubs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subs do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+    end
+    add_index :subs, :title
+  end
+end
