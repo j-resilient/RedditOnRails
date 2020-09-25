4582ff489820c6142761ade38eb30cf184dd7010
J Resilient <j.resilient.1@gmail.com>
2020-09-24 20:37:03 -0400

    Fix User#find_by_credentials to search for the user by username instead of just name

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
     end
 
     def self.find_by_credentials(username, password)
-        user = User.find_by(name: username)
+        user = User.find_by(username: username)
         return nil if user.nil?
         user.is_password?(password) ? user : nil
     end
