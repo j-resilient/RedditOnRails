44376102d78273e2a5f27a323b1bb197ba2d46b0
J Resilient <j.resilient.1@gmail.com>
2020-09-25 19:57:31 -0400

    Add basic validations and moderator association to sub.rb

---
+++ app/models/sub.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: subs
+#
+#  id           :bigint           not null, primary key
+#  title        :string           not null
+#  description  :text             not null
+#  moderator_id :integer          not null
+#
+class Sub < ApplicationRecord
+    validates :title, :description, :moderator_id, presence: true
+
+    belongs_to :moderator,
+        primary_key: :id,
+        foreign_key: :moderator_id,
+        class_name: :User
+end
