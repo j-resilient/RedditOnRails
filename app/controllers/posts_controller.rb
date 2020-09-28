98294c467ea2cd3a126ccad7c802e16151a4ab28
J Resilient <j.resilient.1@gmail.com>
2020-09-28 19:37:30 -0400

    Add #edit and #update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,21 @@ class PostsController < ApplicationController
         render :show
     end
 
+    def edit
+        @post = Post.find_by(id: params[:id])
+        render :edit
+    end
+
+    def update
+        @post = Post.find_by(id: params[:id])
+        if @post.update(post_params)
+            redirect_to post_url(@post)
+        else
+            flash.now[:errors] = @post.errors.full_messages
+            render :edit
+        end
+    end
+
     private
     def post_params
         params.require(:post).permit(:title, :url, :content, :sub_id)
