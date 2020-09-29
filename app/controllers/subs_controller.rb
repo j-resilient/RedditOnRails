e44aef2f23695cf2a5279080ac8504f93d400e62
J Resilient <j.resilient.1@gmail.com>
2020-09-29 15:38:07 -0400

    Fix #create in the SubsController to call #current_user instead of trying to use @current_user

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -15,7 +15,7 @@ class SubsController < ApplicationController
 
     def create
         @sub = Sub.new(sub_params)
-        @sub.moderator_id = @current_user.id
+        @sub.moderator_id = current_user.id
         if @sub.save!
             render :show
         else
