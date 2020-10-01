0f803a4c227f8bdf84ea91973a373f2953bda7e7
J Resilient <j.resilient.1@gmail.com>
2020-10-01 18:01:36 -0400

    Change CommentsController  to be able to create child comments from the comment show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+    before_action :require_login, only: [:create]
     def new
         @post_id = params[:post_id]
         render :new
@@ -7,16 +8,24 @@ class CommentsController < ApplicationController
     def create
         comment = Comment.new(comment_params)
         comment.author_id = current_user.id
+        # if child comment, set post_id
+        comment.post_id = comment.parent_comment.post_id unless comment_params[:post_id]
+        post = comment.post_id
         if comment.save
-            redirect_to post_url(comment_params[:post_id])
+            redirect_to post_url(post)
         else
             flash.now[:errors] = comment.errors.full_messages
             render :new
         end
     end
 
+    def show
+        @parent_id = params[:id]
+        render :show
+    end
+
     private
     def comment_params
-        params.require(:comment).permit(:content, :post_id)
+        params.require(:comment).permit(:content, :post_id, :parent_comment_id)
     end
 end
