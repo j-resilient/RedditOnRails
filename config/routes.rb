dd0e79e91e4a9837cac0100a883821021ee46100
J Resilient <j.resilient.1@gmail.com>
2020-10-15 17:09:13 -0400

    Add upvote and downvote member routes to posts resources

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :subs, except: [:destroy]
   resources :posts, except: [:index] do
     resources :comments, only: [:new]
+    member do
+      post 'upvote'
+      post 'downvote'
+    end
   end
   resources :comments, only: [:create, :show]
 end
